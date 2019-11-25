c0b07566b891f1354c4d9d88b388360052da28ee
Janek <jansmikolajczak@gmail.com>
2019-11-25 13:53:04 +0100

    added method getPersonById and idCheck

--- src/main/java/pl/janmikolajczak/repository/PersonsRepository.java
+++ src/main/java/pl/janmikolajczak/repository/PersonsRepository.java
@@ -9,6 +9,27 @@ public class PersonsRepository {
     private static List<Person> personsList = new ArrayList<>();
 
     static {
-        Person p1 = new Person(1, 70, 1.75, "male", 20);
+        Person p1 = new Person(1, 175, 70, "Male", 20);
+
+        personsList.add(p1);
+    }
+
+    public static List<Person> getPersonById(int id) {
+        List<Person> result = new ArrayList<>();
+        for (Person person : personsList) {
+            if (person.getId() == id) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+
+    public static boolean idCheck(int id) {
+        for (Person person : personsList) {
+            if (person.getId() == id) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/pl/janmikolajczak/service/CalculateBmi.java
+++ src/main/java/pl/janmikolajczak/service/CalculateBmi.java
@@ -4,7 +4,7 @@ public class CalculateBmi {
 
     public static double yourBmi(double weight, double height) {
         double result;
-        result = weight / (height * 2);
+        result = (weight / (height * 2)) * 100;
         return result;
     }
 
